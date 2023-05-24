ef1f35459709c24dfa6d6ea5bb4395a906aa85a2
Olihahaxd <oliverkjoeller12@gmail.com>
2023-05-24 12:44:28 +0200

    finished product

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get update -y
+sudo apt-get install python3-venv -y
+python3 -m venv env
+source ./env/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+deactivate
---
+++ validate.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source ./env/bin/activate
+python src/validate.py
+deactivate
